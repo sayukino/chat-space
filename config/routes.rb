075885b007c739e2c223bc347219511af3daf15f
sayukino <lyulyuki@yahoo.co.jp>
2019-11-15 22:54:19 +0900

    group-function step2
    #WHAT
    setting of the routing
    
    #WHY
    To set the routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
-  resources :users, only: [:edit, :update]
+  root 'messages#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
 
