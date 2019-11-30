32a579dcc85a513f42ce7c9f11f4bff45752f6c7
sayukino <lyulyuki@yahoo.co.jp>
2019-11-30 16:23:47 +0900

    修正しました
    ＃WHAT
    public/uploadsをgitignoreに入れました
    
    ＃WHY
    提出のため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -29,8 +29,8 @@ $(function(){
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      url: url, 
+      type: 'POST',  
       data: formData,  
       dataType: 'json',
       processData: false,
