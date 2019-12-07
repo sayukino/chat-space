44c069e9ac44d1d861486cccc34cf1d9d809f0c5
sayukino <lyulyuki@yahoo.co.jp>
2019-12-07 15:19:42 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -69,6 +69,10 @@ $(function(){
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
           $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        })
+        .fail(function() {
+          alert('error');
       })
     }
   }
