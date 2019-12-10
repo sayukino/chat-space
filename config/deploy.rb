c815abf058cf0e105b13b71af47e3f07841de198
sayukino <lyulyuki@yahoo.co.jp>
2019-12-11 00:12:22 +0900

    capistranoデプロイのための本番環境変数設定
    #WHAT
    capistranoでデプロイするための環境変数の設定
    ＃WHY
    capistranoでデプロイするため

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,14 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
 
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
+
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -25,10 +33,35 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
+# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+#after 'deploy:publishing', 'deploy:restart'
+#namespace :deploy do
+ # task :restart do
+  #  invoke 'unicorn:restart'
+  #end
+#end
+
+
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '<3.115.50.60>', user: 'ec2-user', roles: %w{app db web}
+server '3.115.50.60', user: 'ec2-user', roles: %w{app db web}
 
